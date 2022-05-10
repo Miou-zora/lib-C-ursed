5e5249c29bd9c2c6f0554db883f965ec4dc48d77
miou-zora <alexandre.franquet@epitech.eu>
2022-05-10 19:28:58 +0200

    FIX : calloc doesn't init variables

--- lib/my/fonctions/my_calloc.c
+++ lib/my/fonctions/my_calloc.c
@@ -11,14 +11,17 @@
 void *my_memset(void *, int, int);
 bool function_for_void_function(void(*function)(void *), void *its_a_trap);
 
-void *my_calloc(int elem_count, int elem_size)
+void *r1_my_calloc(int elem_count, int elem_size, char *ptr)
 {
-    void *ptr = NULL;
-
     return ((elem_count == 0 || elem_size == 0) ?
     NULL : ((ptr = malloc(elem_count * elem_size)) == NULL) ?
     NULL : (my_memset(ptr, 0, elem_count * elem_size) == NULL) ?
         (function_for_void_function(&free, ptr)) ?
         NULL : NULL :
     ptr);
+}
+
+void *my_calloc(int elem_count, int elem_size)
+{
+    return (r1_my_calloc(elem_count, elem_size, NULL));
 }
