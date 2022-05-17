5bdcf9729eb8df887eae403a9b182fe6d8a94824
miou-zora <alexandre.franquet@epitech.eu>
2022-05-17 23:45:00 +0200

    test

---
+++ lib/my/tests/test_get_nbrlen.c
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2022
+** libmy
+** File description:
+** test_get_nbrlen
+*/
+
+#include <criterion/criterion.h>
+
+unsigned int get_nbrlen(int nb);
+
+Test(get_nbrlen, pos)
+{
+    int result = get_nbrlen(999);
+    int expected = 3;
+
+    cr_assert_eq(result, expected);
+}
+
+Test(get_nbrlen, zero)
+{
+    int result = get_nbrlen(0);
+    int expected = 0;
+
+    cr_assert_eq(result, expected);
+}
+
+Test(get_nbrlen, neg)
+{
+    int result = get_nbrlen(-999);
+    int expected = 3;
+
+    cr_assert_eq(result, expected);
+}
+
+Test(get_nbrlen, little_neg)
+{
+    int result = get_nbrlen(-1);
+    int expected = 1;
+
+    cr_assert_eq(result, expected);
+}
+
+Test(get_nbrlen, little_pos)
+{
+    int result = get_nbrlen(1);
+    int expected = 1;
+
+    cr_assert_eq(result, expected);
+}
