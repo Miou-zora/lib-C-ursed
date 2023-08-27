928e912b26214754ce245d8b7ca5272e6f9dbd13
Miouzora <alexandre.franquet@epitech.eu>
2023-08-27 21:38:24 +0200

    les commits de ce projet sont horrible, sad a moi

--- lib/my/fonctions/data_to_array_str.c
+++ lib/my/fonctions/data_to_array_str.c
@@ -7,12 +7,14 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int my_strlen(char const *str);
 void *my_calloc(int elem_count, int elem_size);
 int my_printf(char const *str, ...);
+char *my_strdup(char const *src);
 
-bool is_char_is_that(char c, char const *array)
+bool is_char_is_that(char c, const char *array)
 {
     return ((array == NULL || *array == '\0') ?
     false : (*array == c) ?
@@ -20,7 +22,7 @@ bool is_char_is_that(char c, char const *array)
     true : false);
 }
 
-unsigned int number_of_word_start(char const *str, char *sep)
+unsigned int number_of_word_start(const char *str, const char *sep)
 {
     return ((sep == NULL || str == NULL) ?
     0 : (*str == '\0') ?
@@ -29,25 +31,30 @@ unsigned int number_of_word_start(char const *str, char *sep)
         1 : 0) + (number_of_word_start(str + 1, sep)));
 }
 
-char **data_to_array_str(char *new, char *sep)
+char **data_to_array_str(const char *new, const char *sep)
 {
     int len_all = my_strlen(new);
     int tmp = number_of_word_start(new, sep) + 2;
     char **array = my_calloc(tmp, sizeof(*array));
+    char *new_dup = NULL;
     int nbr_ptr = 0;
     int i = 0;
 
+    if (!new || !sep)
+        return (NULL);
+    new_dup = my_strdup(new);
     for (; i < len_all; i++) {
         if (is_char_is_that(new[i], sep)) {
-            new[i] = '\0';
+            new_dup[i] = '\0';
         }
     }
     for (i = 0; i < len_all; i++) {
-        if (i == 0 || (!new[i - 1] && !is_char_is_that(new[i], sep))) {
-            array[nbr_ptr] = &new[i];
+        if (i == 0 || (!new_dup[i - 1] && !is_char_is_that(new_dup[i], sep))) {
+            array[nbr_ptr] = my_strdup(&new_dup[i]);
             nbr_ptr = nbr_ptr + 1;
         }
     }
+    free(new_dup);
     array[nbr_ptr] = NULL;
     return (array);
 }
---
+++ lib/my/tests/test_data_to_array_str.c
@@ -0,0 +1,132 @@
+/*
+** EPITECH PROJECT, 2023
+** lib-C-ursed [WSL : Ubuntu]
+** File description:
+** test_data_to_array_str
+*/
+
+#include <criterion/criterion.h>
+
+char **data_to_array_str(char *new, char *sep);
+
+Test(data_to_array_str, null_null)
+{
+    char **result = data_to_array_str(NULL, NULL);
+
+    cr_assert_null(result);
+}
+
+Test(data_to_array_str, null_value)
+{
+    char *sep_value = "test";
+    char **result = data_to_array_str(NULL, sep_value);
+
+    cr_assert_null(result);
+}
+
+Test(data_to_array_str, value_null)
+{
+    char *new_value = "test";
+    char **result = data_to_array_str(new_value, NULL);
+
+    cr_assert_null(result);
+}
+
+Test(data_to_array_str, casual)
+{
+    char *new_value = "test1;test2;test3";
+    char *sep_value = ";";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {"test1", "test2", "test3", NULL};
+
+    cr_assert_not_null(result);
+    for (int i = 0; expected[i]; i++)
+        cr_assert_str_eq(result[i], expected[i]);
+    for (int i = 0; result[i]; i++)
+        free(result[i]);
+    free(result);
+}
+
+Test(data_to_array_str, no_separator)
+{
+    char *new_value = "test1test2test3";
+    char *sep_value = ";";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {"test1test2test3", NULL};
+
+    cr_assert_not_null(result);
+    for (int i = 0; expected[i]; i++)
+        cr_assert_str_eq(result[i], expected[i]);
+    for (int i = 0; result[i]; i++)
+        free(result[i]);
+    free(result);
+}
+
+Test(data_to_array_str, empty_new)
+{
+    char *new_value = "";
+    char *sep_value = ";";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {NULL};
+
+    cr_assert_not_null(result);
+    cr_assert_null(result[0]);
+    free(result);
+}
+
+Test(data_to_array_str, empty_sep)
+{
+    char *new_value = "test1;test2;test3";
+    char *sep_value = "";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {"test1;test2;test3", NULL};
+
+    cr_assert_not_null(result);
+    for (int i = 0; expected[i]; i++)
+        cr_assert_str_eq(result[i], expected[i]);
+    for (int i = 0; result[i]; i++)
+        free(result[i]);
+    free(result);
+}
+
+Test(data_to_array_str, empty_empty)
+{
+    char *new_value = "";
+    char *sep_value = "";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {NULL};
+
+    cr_assert_not_null(result);
+    cr_assert_null(result[0]);
+    free(result);
+}
+
+Test(data_to_array_str, two_different_sep)
+{
+    char *new_value = "test1;test2.test3/test4";
+    char *sep_value = ";.";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {"test1", "test2", "test3/test4", NULL};
+
+    cr_assert_not_null(result);
+    for (int i = 0; expected[i]; i++)
+        cr_assert_str_eq(result[i], expected[i]);
+    for (int i = 0; result[i]; i++)
+        free(result[i]);
+    free(result);
+}
+
+Test(data_to_array_str, two_same_sep)
+{
+    char *new_value = "test1;;;test2;test3;test4";
+    char *sep_value = ";;";
+    char **result = data_to_array_str(new_value, sep_value);
+    char *expected[] = {"test1", "", "", "test2", "test3", "test4", NULL};
+
+    cr_assert_not_null(result);
+    for (int i = 0; expected[i]; i++)
+        cr_assert_str_eq(result[i], expected[i]);
+    for (int i = 0; result[i]; i++)
+        free(result[i]);
+    free(result);
+}
